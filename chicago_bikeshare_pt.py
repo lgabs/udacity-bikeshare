eec9c21b926b2a169cc76df4776f7488ed1ea9ac
lgabs <luangabriel70@gmail.com>
2018-07-04 22:55:26 -0300

    tarefas de 1 a 6 completas

--- chicago_bikeshare_pt.py
+++ chicago_bikeshare_pt.py
@@ -3,11 +3,13 @@
 # Começando com os imports
 import csv
 import matplotlib.pyplot as plt
+import sys
 
-# Vamos ler os dados como uma lista
+# Vamos ler os dados como uma lista de dicionários
 print("Lendo o documento...")
 with open("chicago.csv", "r") as file_read:
-    reader = csv.reader(file_read)
+    #reader = csv.reader(file_read)
+    reader = csv.DictReader(file_read) 
     data_list = list(reader)
 print("Ok!")
 
@@ -18,9 +20,11 @@ print(len(data_list))
 # Imprimindo a primeira linha de data_list para verificar se funcionou.
 print("Linha 0: ")
 print(data_list[0])
-# É o cabeçalho dos dados, para que possamos identificar as colunas.
+# É o cabeçalho dos dados, para que possamos identificar as colunas. Se a lista é uma lista
+# de dicionários, então esta linha será a primeira viagem e suas informações. 
 
-# Imprimindo a segunda linha de data_list, ela deveria conter alguns dados
+# Imprimindo a segunda linha de data_list, ela deveria conter alguns dados. Se a lista é 
+# uma lista de dicionários, então esta será a segunda viagem e suas informações.
 print("Linha 1: ")
 print(data_list[1])
 
@@ -29,17 +33,38 @@ input("Aperte Enter para continuar...")
 # TODO: Imprima as primeiras 20 linhas usando um loop para identificar os dados.
 print("\n\nTAREFA 1: Imprimindo as primeiras 20 amostras")
 
+for k in range(20):
+    #print(data_list[k])
+    print(list(data_list[k].values()))
+
+
 # Vamos mudar o data_list para remover o cabeçalho dele.
-data_list = data_list[1:]
+# data_list = data_list[1:]
 
 # Nós podemos acessar as features pelo índice
 # Por exemplo: sample[6] para imprimir gênero, ou sample[-2]
+
+# Sendo uma lista de dicionários, cada índice da lista é uma viagem registrada em forma de
+# dicionário. Podemos acessar as features usando os seus próprios nomes como chaves
+# Exemplo: sample = data_list[k] retorna a k-ésima viagem. sample['Gender'] retorna o gênero.
+
+# As features são:
+# ['Start Time', 'End Time', 'Trip Duration', 'Start Station', 'End Station', 'User Type', 'Gender', 'Birth Year']
+
 
 input("Aperte Enter para continuar...")
 # TAREFA 2
 # TODO: Imprima o `gênero` das primeiras 20 linhas
 
 print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
+
+# Impressão direta
+#for k in range(20):
+#    print(data_list[k]['Gender'])
+
+# Impressão com a numeração para conferir as 20 amostras
+for i, d in enumerate(data_list[:20]):
+    print('{}: {}'.format(i+1,d['Gender']))
 
 
 # Ótimo! Nós podemos pegar as linhas(samples) iterando com um for, e as colunas(features) por índices.
@@ -48,9 +73,17 @@ print("\nTAREFA 2: Imprimindo o gênero das primeiras 20 amostras")
 input("Aperte Enter para continuar...")
 # TAREFA 3
 # TODO: Crie uma função para adicionar as colunas(features) de uma lista em outra lista, na mesma ordem
+
+# Solução: já que se optou por uma lista de dicionários, ao ínves do índice usamos diretamente o label da feature desejada.
+# Para não alterar o tratamento dos asserts, criou-se uma lista de fetaures para associar o índice de entrada com a respectiva feature 
 def column_to_list(data, index):
+    # data:  lista de dicionários com as mesmas chaves em cada
+    # index: índice que representa uma feature
     column_list = []
+    features = list(data[0].keys())
+    #print(features)  
     # Dica: Você pode usar um for para iterar sobre as amostras, pegar a feature pelo seu índice, e dar append para uma lista
+    column_list = [trip[features[index]] for trip in data]
     return column_list
 
 
@@ -64,30 +97,47 @@ assert len(column_to_list(data_list, -2)) == 1551505, "TAREFA 3: Tamanho incorre
 assert column_to_list(data_list, -2)[0] == "" and column_to_list(data_list, -2)[1] == "Male", "TAREFA 3: A lista não coincide."
 # -----------------------------------------------------
 
+
+
 input("Aperte Enter para continuar...")
 # Agora sabemos como acessar as features, vamos contar quantos Male (Masculinos) e Female (Femininos) o dataset tem
 # TAREFA 4
 # TODO: Conte cada gênero. Você não deveria usar uma função parTODO isso.
+
+# Solução: Se não se deve utilizar uma função direta para calcular isso, vamos proceder assim:
+# Ao invés de percorrer a lista de dicionários, podemos percorrer diretamente a lista de gêneros com a função anterior
 male = 0
 female = 0
+for trip in data_list:
+    if trip['Gender'] == 'Male':
+        male += 1
+    elif trip['Gender'] == 'Female':
+        female += 1
 
 
 # Verificando o resultado
 print("\nTAREFA 4: Imprimindo quantos masculinos e femininos nós encontramos")
-print("Masculinos: ", male, "\nFemininos: ", female)
+print("Masculinos: ", male, "\nFemininos:  ", female)
 
 # ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
 assert male == 935854 and female == 298784, "TAREFA 4: A conta não bate."
 # -----------------------------------------------------
+
 
 input("Aperte Enter para continuar...")
 # Por que nós não criamos uma função parTODO isso?
 # TAREFA 5
 # TODO: Crie uma função para contar os gêneros. Retorne uma lista.
 # Isso deveria retornar uma lista com [count_male, count_female] (exemplo: [10, 15] significa 10 Masculinos, 15 Femininos)
-def count_gender(data_list):
+def count_gender(data):
+    # data: lista de dicionários 
     male = 0
     female = 0
+    for trip in data:
+        if trip['Gender'] == 'Male':
+            male += 1
+        elif trip['Gender'] == 'Female':
+            female += 1
     return [male, female]
 
 
@@ -105,8 +155,16 @@ input("Aperte Enter para continuar...")
 # TAREFA 6
 # TODO: Crie uma função que pegue o gênero mais popular, e retorne este gênero como uma string.
 # Esperamos ver "Masculino", "Feminino", ou "Igual" como resposta.
-def most_popular_gender(data_list):
+def most_popular_gender(data):
+    # data: lista de dicionários 
     answer = ""
+    n_genders = count_gender(data_list)  # gerar a lista com número de homens e mulheres com função já desenvolvida
+    if n_genders[0] > n_genders[1]:
+        answer += 'Masculino'
+    elif n_genders[0] < n_genders[1]:
+        answer += 'Feminino'
+    else:
+        answer += 'Igual'
     return answer
 
 
@@ -117,7 +175,7 @@ print("O gênero mais popular na lista é: ", most_popular_gender(data_list))
 assert type(most_popular_gender(data_list)) is str, "TAREFA 6: Tipo incorreto no retorno. Deveria retornar uma string."
 assert most_popular_gender(data_list) == "Masculino", "TAREFA 6: Resultado de retorno incorreto!"
 # -----------------------------------------------------
-
+sys.exit()
 # Se tudo está rodando como esperado, verifique este gráfico!
 gender_list = column_to_list(data_list, -2)
 types = ["Male", "Female"]
@@ -186,7 +244,7 @@ assert len(user_types) == 582, "TAREFA 10: Comprimento errado de start stations.
 # -----------------------------------------------------
 
 input("Aperte Enter para continuar...")
-# TAREFA 11
+'''# TAREFA 11
 # Volte e tenha certeza que você documenteou suas funções. Explique os parâmetros de entrada, a saída, e o que a função faz. Exemplo:
 # def new_function(param1: int, param2: str) -> list:
       """
@@ -198,7 +256,7 @@ input("Aperte Enter para continuar...")
           Uma lista de valores x.
 
       """
-
+'''
 input("Aperte Enter para continuar...")
 # TAREFA 12 - Desafio! (Opcional)
 # TODO: Crie uma função para contar tipos de usuários, sem definir os tipos
